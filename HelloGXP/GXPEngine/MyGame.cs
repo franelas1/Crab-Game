b14ef3b4baa1b3c902ed1777d3dc002732a7882e
franelas1 <102754586+franelas1@users.noreply.github.com>
2024-02-15 19:31:47 +0100

    Some pregress

--- HelloGXP/GXPEngine/MyGame.cs
+++ HelloGXP/GXPEngine/MyGame.cs
@@ -1,43 +1,29 @@
 using System;                   // System contains a lot of default C# libraries 
 using GXPEngine;                // GXPEngine contains the engine
 
-public class MyGame : Game {
-	// Declare the Sprite variables:
-	Sprite spaceShip;
-	AnimationSprite character;
-	EasyDraw background;
-	EasyDraw button;
-	EasyDraw information;
-	//add some comments
-	//test1
-
+public class MyGame : Game
+{
 	// Declare other variables:
-
-	float turnSpeedShip = 3;
-	float moveSpeedShip = 5;
-	float characterSpeed = 3;
-
-	SoundChannel soundTrack;*/
-	Player player1;
+	Player player1, player2;
+	Platform plat1;
 	public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
 	{
+		//Spawning and adding players
 		player1 = new Player(width/2, height);
 		AddChild(player1);
 
+		//player2 = new Player(width - 200, height / 2);
+		//AddChild(player2);
 
+		//test platform
+		plat1 = new Platform();
+		AddChild(plat1);
 	}
-	void mainMenuScreen()
-    {
-		
-    }
-
-	void doGameLoop()
-    {
-
-    }
 
 	void Update() {
-		player1.updatePlayer();
+		//Updating player movement with human imput
+		player1.updatePlayer(Input.GetKey(Key.D), Input.GetKey(Key.A), Input.GetKeyDown(Key.W));
+		//player2.updatePlayer(Input.GetKey(Key.RIGHT), Input.GetKey(Key.LEFT), Input.GetKeyDown(Key.UP));
 	}
 
 	// Main is the first method that's called when the program is run
--- HelloGXP/GXPEngine/Player.cs
+++ HelloGXP/GXPEngine/Player.cs
@@ -1,69 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace GXPEngine
+﻿namespace GXPEngine
 {
     class Player : Sprite
     {
+        //Variables
         bool ableToJump;
 
         float speedX = 4;
         float speedY;
+        //Player constructor (coordinate X, coordinate Y) : (sprite)     will need to change to animated sprite 
         public Player(float tempX, float tempY) : base("circle.png")
         {
+            //Setting player origin at the middle of bottom side
             SetOrigin(width / 2, height);
             x = tempX;
             y = tempY;
         }
 
-        public void updatePlayer()
+        //Updating player movement. Takes in 3 inputs (for now) for each right, left and up buttons.
+        public void updatePlayer(bool right, bool left, bool up)
         {
-            movementLR(Input.GetKey(Key.D), Input.GetKey(Key.A));
-            movementUD(Input.GetKeyDown(Key.W));
-            
+            movementLR(right, left);
+            movementUD(up);
+
         }
 
+        //Player movement Left Right
         void movementLR(bool goRight, bool goLeft)
         {
-            if(goRight)
+            //If right button pressed 
+            if (goRight)
             {
                 x += speedX;
-                if(x + width / 2 > game.width)
+                if (x + width / 2 > game.width)
                 {
                     x -= speedX;
                 }
             }
-            if(goLeft)
+
+            //If left button pressed
+            if (goLeft)
             {
                 x -= speedX;
-                if(x - width / 2 < 0)
+                if (x - width / 2 < 0)
                 {
                     x += speedX;
                 }
             }
         }
 
+        //Player movement Up Down
         void movementUD(bool jump)
         {
-            speedY += 0.15f;
+            //Always tries to move down
+            speedY += 0.1f;
             y += speedY;
-            
-            if(y > game.height)
+
+            //If below floor go back, reset falling speed and enable jump again 
+            if (y > game.height)
             {
                 y -= speedY;
+                speedY = 0;
                 ableToJump = true;
             }
-
-            if(jump && ableToJump)
+            else
             {
-                speedY = -10;
                 ableToJump = false;
             }
-            
+
+            //If able to jump and jump button is pressed, move up and disable jump
+            if (jump && ableToJump)
+            {
+                speedY = -8;
+            }
+
         }
 
-        
+
     }
 }
