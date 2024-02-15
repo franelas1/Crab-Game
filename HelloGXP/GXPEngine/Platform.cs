0f7acc4e3c19824c909a6b8546fc7857e5368cff
franelas1 <franelas@wp.pl>
2024-02-15 22:09:52 +0100

    Platforms wait for the first jump

--- HelloGXP/GXPEngine/Platform.cs
+++ HelloGXP/GXPEngine/Platform.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace GXPEngine
+﻿namespace GXPEngine
 {
     class Platform : Sprite
     {
         int margin = 50; // how many pixels away the walls are from the sides of the screen
-        public Platform() : base("square.png")
+        public Platform(float tempY) : base("square.png")
         {
             SetOrigin(width / 2, 0);
             scaleX = Utils.Random(2f, 3f);
             scaleY = 0.5f;
             x = Utils.Random(margin + width, game.width - margin - width);
-            y = 200;
+            y = tempY;
         }
 
         public void updatePlatform()
@@ -36,5 +31,6 @@ namespace GXPEngine
                 this.Destroy();
             }
         }
+
     }
 }
