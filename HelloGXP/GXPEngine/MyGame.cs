b138fcb58116729df203f693eec1b94db43578f3
franelas1 <franelas@wp.pl>
2024-02-26 15:58:48 +0100

    Arduino update

--- HelloGXP/GXPEngine/MyGame.cs
+++ HelloGXP/GXPEngine/MyGame.cs
@@ -2,7 +2,7 @@ using GXPEngine;                    // GXPEngine contains the engine
 using System;
 using System.Collections.Generic;   // Adding lists
 using System.Runtime.Remoting.Activation;
-//using System.IO.Ports;
+using System.IO.Ports;
 
 public class MyGame : Game
 {
