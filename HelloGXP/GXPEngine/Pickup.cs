3142e7e427240fca141da348d73c4fccad032be0
SeanBos03 <142151673+SeanBos03@users.noreply.github.com>
2024-02-28 14:56:40 +0100

    update

---
+++ HelloGXP/GXPEngine/Pickup.cs
@@ -0,0 +1,21 @@
+﻿using GXPEngine.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine
+{
+    public class Pickup : Sprite
+    {
+        public string theAbility;
+        public Pickup(float posX, float posY, string filename, string theAbility) : base(filename)
+        {
+            x = posX;
+            y = posY;
+            this.theAbility = theAbility;
+        }
+    }
+}
